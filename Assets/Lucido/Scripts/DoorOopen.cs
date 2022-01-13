1e8be7f4f94b2ba7553a66cc5c24fe73e4370f2e
Osled <50557990+Osled@users.noreply.github.com>
2022-01-13 13:14:44 -0800

    Update code

---
+++ Assets/Lucido/Scripts/DoorOopen.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOopen : MonoBehaviour
+{
+    public GameObject doorLock;
+    public GameObject key;
+    public Animator m_Animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="Key")
+        {
+           // key.GetComponent<DistanceGrabbable>().enabled = false;
+            key.transform.position= doorLock.transform.position;
+            key.transform.rotation= doorLock.transform.rotation;
+
+            key.GetComponent<Rigidbody>().useGravity =false;
+            key.GetComponent<Rigidbody>().isKinematic =true;
+            m_Animator.SetBool("Open", true);
+        }
+    }
+}
